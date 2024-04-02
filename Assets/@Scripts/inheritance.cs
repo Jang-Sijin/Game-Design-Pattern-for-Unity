b043dfc43be73ab16bffec65d916e8920881ca4a
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2024-04-02 23:51:45 +0900

    디자인 패턴 개요
    디자인 패턴은 하위 클래스 객체를 상위 클래스 객체에 대입할 수 있다는 대전제 하에서 디자인 패턴이 시작된다.
    1. 하위(자식) 클래스 객체는 상위(부모) 클래스 객체에 대입이 가능하다.
    2. 상위(부모) 클래스 객체는 하위(자식)클래스 객체에 대입이 불가능하다.
    3. 파생 클래스에서 재정의한 멤버 함수를 기본(Base) 클래스의 포인터로 호출하면 일반적으로 재정의 전의 기본(Base) 클래스의 멤버 함수가 호출된다.
    파생 클래스에서 재정의한 후의 멤버 함수가 호출되도록 하기 위해서는 가상(Virtual) 함수로 정의해야 한다.

---
+++ Assets/@Scripts/inheritance.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Super
+{
+    private string name;
+    private int age;
+
+    private int GetAge()
+    {
+        return age;
+    }
+}
+
+public class Sub : Super
+{
+    
+}
